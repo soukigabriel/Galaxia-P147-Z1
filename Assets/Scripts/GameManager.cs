fb838ebc025dfabcdfa6ecb9278a20d66aa92f79
Gabriel Souki <gabrielsouki@gmail.com>
2022-05-23 13:40:15 -0400

    Se añadio el modelo del personaje con sus texturas y una plataforma para prototipar las mecanicas. Se creo la mecanica de movimiento del personaje, salto y movimiento de la camara de manera básica.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { mainMenu, pause, inGame, gameOver };
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager sharedInstance;
+    public GameState currentGameState;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Mas adelante este metodo debe cambiarse a MainMenu();
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MainMenu()
+    {
+        SetGameState(GameState.mainMenu);
+    }
+
+    public void PauseGame()
+    {
+        SetGameState(GameState.pause);
+    }
+
+    public void StartGame()
+    {
+        SetGameState(GameState.inGame);
+    }
+
+    public void GameOver()
+    {
+        SetGameState(GameState.gameOver);
+    }
+
+    void SetGameState(GameState newGameState)
+    {
+        switch (newGameState)
+        {
+            case GameState.mainMenu:
+                //Implementar logica de menu principal
+                break;
+            case GameState.pause:
+                //Implementar logica de menu de pausa
+                break;
+            case GameState.inGame:
+                //Implementar logica de empezar el juego
+                break;
+            case GameState.gameOver:
+                //Implementar logica de game over
+                break;
+        }
+
+        currentGameState = newGameState;
+    }
+
+    private void Awake()
+    {
+        if(sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
